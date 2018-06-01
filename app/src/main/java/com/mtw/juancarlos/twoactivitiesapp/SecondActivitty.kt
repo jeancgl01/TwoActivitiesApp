67cde24d470d830fc8c944050563df21ed86eb31
juancarlosgallegos <juan.gallegos@bachoco.net>
2018-06-01 16:13:05 -0500

    Initial commit

---
+++ app/src/main/java/com/mtw/juancarlos/twoactivitiesapp/SecondActivitty.kt
@@ -0,0 +1,12 @@
+package com.mtw.juancarlos.twoactivitiesapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivitty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_activitty)
+    }
+}
