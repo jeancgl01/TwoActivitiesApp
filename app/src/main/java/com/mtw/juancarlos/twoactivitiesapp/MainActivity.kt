6eeb4648781a28525ebe64f625d8f95019d83ad1
juancarlosgallegos <juan.gallegos@bachoco.net>
2018-06-06 15:55:19 -0500

    Practica del tema 2.1 Create and start Activities.

--- app/src/main/java/com/mtw/juancarlos/twoactivitiesapp/MainActivity.kt
+++ app/src/main/java/com/mtw/juancarlos/twoactivitiesapp/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.mtw.juancarlos.twoactivitiesapp
 
+import android.app.Activity
+import android.content.Intent
+import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.util.Log
+import android.view.View
+import com.mtw.juancarlos.twoactivitiesapp.R.id.text_message_reply
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity::class.java.simpleName
+
+    companion object {
+        val EXTRA_MESSAGE = "com.mtw.juancarlos.twoactivitiesapp.extra.MESSAGE"
+        val TEXT_REQUEST = 1
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun launchSecondActivity(view: View){
+        Log.d(LOG_TAG,"button clicked!")
+
+        val message = editText_main.text.toString()
+        val intent = Intent(this,SecondActivitty::class.java)
+        intent.putExtra(EXTRA_MESSAGE,message)
+        //startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == TEXT_REQUEST && resultCode == Activity.RESULT_OK ){
+            val reply = data?.getStringExtra(SecondActivitty.EXTRA_REPLY) ?: ""
+            text_header_reply.visibility = View.VISIBLE
+            text_message_reply.text = reply
+            text_message_reply.visibility = View.VISIBLE
+        }
+
     }
 }
--- app/src/main/java/com/mtw/juancarlos/twoactivitiesapp/SecondActivitty.kt
+++ app/src/main/java/com/mtw/juancarlos/twoactivitiesapp/SecondActivitty.kt
@@ -1,12 +1,35 @@
 package com.mtw.juancarlos.twoactivitiesapp
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_second_activitty.*
 
 class SecondActivitty : AppCompatActivity() {
+
+    companion object {
+        val EXTRA_REPLY = "com.mtw.juancarlos.twoactivitiesapp.extra.REPLY"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_activitty)
+
+        //Log.e("SECOND", "VALOR:" + intent.getStringExtra(MainActivity.EXTRA_MESSAGE))
+        text_message.text = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+
     }
+
+    fun returnReply(view:View){
+        val replyString = editText_second.text.toString()
+        val replyIntent = Intent()
+        replyIntent.putExtra(EXTRA_REPLY,replyString)
+        setResult(Activity.RESULT_OK,replyIntent)
+        finish()
+    }
+
 }
